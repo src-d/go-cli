affab546a4106f42ed7f8fdc90920b85fd8a4c73
Santiago M. Mola <santi@mola.io>
2018-07-24 11:54:34 +0200

    add dynamic flags example

---
+++ examples/dynamic/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/kami-zh/go-capturer"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(t *testing.T) {
+	require := require.New(t)
+	os.Args = []string{"test", "--help"}
+	var (
+		stdout, stderr string
+	)
+	stdout = capturer.CaptureStdout(func() {
+		stderr = capturer.CaptureStderr(func() {
+			main()
+		})
+	})
+
+	require.Empty(stderr)
+	require.True(strings.HasPrefix(stdout, "Usage:\n  basic"))
+}
